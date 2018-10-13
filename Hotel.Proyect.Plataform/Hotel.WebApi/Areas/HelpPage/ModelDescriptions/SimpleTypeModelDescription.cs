bdce840b397ab8daf9e45a54e1a921bf0d62247c
Ronald <pgf.ronald20@hotmail.com>
2018-10-13 03:44:54 -0500

    my commit

---
+++ Hotel.Proyect.Plataform/Hotel.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Hotel.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Hotel.Proyect.Plataform/Hotel.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hotel.WebApi.WebApiApplication" Language="C#" %>
