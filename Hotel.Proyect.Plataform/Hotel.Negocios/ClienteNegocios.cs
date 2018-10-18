607da0c6ede610d158ed74f88657c4e452d0a469
Mery <vane17121203@hotmail.com>
2018-10-18 03:51:46 -0500

    Segundo cambio de Vanessa.

---
+++ Hotel.Proyect.Plataform/Hotel.Negocios/ClienteNegocios.cs
@@ -0,0 +1,30 @@
+﻿using Hotel.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocios
+{
+    public class ClienteNegocios
+    {
+        ClienteDatos datosCliente = new ClienteDatos();
+
+        public string LoginCliente(string login, string contraseña)
+        {
+            string mensaje = "";
+            int cuenta = datosCliente.ValidarLoginCliente(login, contraseña);
+            if (cuenta==0)
+            {
+                mensaje = "Login y/o contraseña incorrectos";
+            }
+            else
+            {
+                mensaje = "Ingreso exitoso";
+            }
+            return mensaje;
+        } 
+    }
+
+}
