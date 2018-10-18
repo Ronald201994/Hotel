b3942c14371302f063a5a742c71e2e896b786921
Mery <vane17121203@hotmail.com>
2018-10-17 22:44:19 -0500

    Cambios de Vanessa.

---
+++ Hotel.Proyect.Plataform/Hotel.Datos/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Referencias
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Hotel.Datos
+{
+    public class Conexion
+    {
+        SqlConnection conexion;
+        public SqlConnection Conectar()
+        {
+            conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+            return conexion;
+        }
+    }
+}
