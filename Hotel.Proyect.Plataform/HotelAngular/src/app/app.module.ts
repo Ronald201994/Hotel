02ae7c84d6cf27b5260dca98278d4386d5d4a07c
RMAMANI <ronald.mamani@cignium.com>
2018-10-21 21:06:50 -0500

    -Cambios de Ronald en el Angular - Navigation page

--- Hotel.Proyect.Plataform/HotelAngular/src/app/app.module.ts
+++ Hotel.Proyect.Plataform/HotelAngular/src/app/app.module.ts
@@ -3,20 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BuscarHabitacionByPrecioComponent } from './habitacion/buscarHabitacionByPrecio';
 
 //Animations
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.component';
+import { MaterialModule } from './material.component';''
+import {RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BuscarHabitacionByPrecioComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'buscarHabitacion', component: BuscarHabitacionByPrecioComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
