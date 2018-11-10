21a102c0ef020342ee7402c89ba6e0e79290ffd4
Brayhan <bryhn08@outlook.com>
2018-11-09 23:48:33 -0500

    metodo de pago ..

---
+++ Hotel.Proyect.Plataform/Hotel.Negocios/ValidarPagoNegocios.cs
@@ -0,0 +1,56 @@
+﻿using Hotel.Datos;
+using Hotel.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocios
+{
+    public class ValidarPagoNegocios
+    {
+        TarjetaDatos tarjetaDatos = new TarjetaDatos();
+        public bool ValidarPago(out string mensaje,
+                                int tipoTarjeta,
+                                string numeroTarjeta,
+                                string titularTarjeta,
+                                double montoConsumir,
+                                string mesExpiracion,
+                                string añoExpiracion,
+                                string codigoSeguridad)
+        {
+            bool ValidacionCorrecta = false;
+
+            mensaje = "";
+
+            TarjetaInfo tarjetaInfo = tarjetaDatos.ObtenerInformacionTarjeta(tipoTarjeta, numeroTarjeta, titularTarjeta, 
+                                                    mesExpiracion, añoExpiracion,codigoSeguridad);
+
+            if(tarjetaInfo == null)
+            {
+                mensaje = "Tarjeta no existe";
+            }else
+            {
+                if (!tarjetaInfo.tarjetaHabilitada)
+                {
+                    mensaje = "Tarjeta No Habilitada";
+                }
+                else
+                {
+                    if(tarjetaInfo.creditoDisponible < montoConsumir)
+                    {
+                        mensaje = "Linea de credito insuficiente";
+                    }
+                    else
+                    {
+                        ValidacionCorrecta = true;
+                    }
+                }
+                
+            }
+
+            return ValidacionCorrecta;
+        }
+    }
+}
---
+++ Hotel.Proyect.Plataform/Hotel.WebApi/Controllers/PagoController.cs
@@ -0,0 +1,30 @@
+﻿using Hotel.Negocios;
+using Hotel.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hotel.WebApi.Controllers
+{
+    public class PagoController : ApiController
+    {
+        ValidarPagoNegocios negocios = new ValidarPagoNegocios();
+        [HttpPost]
+        public ValidarPagoResponse ValidarPago(ValidarPagoRequest request)
+        {
+            ValidarPagoResponse response = new ValidarPagoResponse();
+            string mensaje = "";
+            response.TransaccionCompleta = negocios.ValidarPago(out mensaje,
+                                           request.TipoTarjeta, request.NumeroTarjeta,
+                                           request.TitularTarjeta, request.MontoConsumir,
+                                           request.MesExpiracionTarjeta, request.AñoExpiracionTarjeta,
+                                           request.CodigoSeguridadTarjeta);
+            response.TransaccionMensaje = mensaje;
+
+            return response;
+        }
+    }
+}
