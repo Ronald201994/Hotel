28192a3e0ec895807d9383af2660459cda5157ee
Brayhan <bryhn08@outlook.com>
2018-11-12 17:43:14 -0500

    validaciones pasarela

--- Hotel.Proyect.Plataform/HotelAngular/src/app/pasarelaPago/pasarela.component.ts
+++ Hotel.Proyect.Plataform/HotelAngular/src/app/pasarelaPago/pasarela.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PasarelaServicios } from './servicio.pasarela';
 import { Pasarela } from './pasarela';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector : '',
@@ -8,10 +9,27 @@ import { Pasarela } from './pasarela';
     styleUrls: ['./pago.css']
 })
 
-export class PasarelaComponent { 
-    pasarela : Pasarela = null;
+export class PasarelaComponent implements OnInit { 
 
-    constructor(private _pasarela: PasarelaServicios){
+    formPasarela : FormGroup;
+    submitted = false;
+   
+    ngOnInit(): void{
+        this.formPasarela = this.formBuilder.group({
+            tarjeta:['',Validators.required, Validators.minLength(16), Validators.maxLength(16)],
+            titular:['',Validators.required],
+            mes:['',Validators.required],
+            year:['',Validators.required],
+            codSeguridad:['',Validators.required, Validators.minLength(3), Validators.maxLength(3) ],
+            monto:['',Validators.required]
+        });
+    }
+
+    get f() {return this.formPasarela.controls;}
+
+    pasarela: Pasarela = null;
+
+    constructor(private _pasarela: PasarelaServicios , private formBuilder: FormBuilder){
         this.pasarela = <Pasarela>{
             NumeroTarjeta:"",
             TipoTarjeta:"",
@@ -24,8 +42,17 @@ export class PasarelaComponent {
     }
 
     ingresePasarela(): void{
-        var pasarela = this._pasarela.ingresePasarela(this.pasarela)
+
+        this.submitted = true;
+
+        if(this.formPasarela.invalid){
+            return;
+        }else{
+            this._pasarela.ingresePasarela(this.pasarela)
         .subscribe();
+        }
+
+        
     }
 
 }
